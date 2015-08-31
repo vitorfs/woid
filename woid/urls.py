2ca583ef7fb35903838c8263dfef714029baca09
Vitor Freitas <vitorfs@gmail.com>
2015-08-31 23:35:18 +0300

    remove url bug

--- woid/urls.py
+++ woid/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', 'woid.apps.core.views.home', name='home'),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', { 'next_page': '/' }, name='logout'),
-    url(r'^signup/', CreateView.as_view(template_name='registration/signup.html', form_class=UserCreationForm, success_url='/plans/'), name='signup'),
+    url(r'^signup/', CreateView.as_view(template_name='registration/signup.html', form_class=UserCreationForm, success_url='/'), name='signup'),
     url(r'^(?P<slug>[-_\w]+)/', include('woid.apps.services.urls', namespace='services')),
     url(r'^admin/', include(admin.site.urls)),
 )
