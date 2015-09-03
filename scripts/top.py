6f073d2602aa948a6a929a3318c728362a103d9a
Vitor Freitas <vitorfs@gmail.com>
2015-09-03 16:24:51 +0300

    Update services for easier maintenance

---
+++ scripts/top.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+from unipath import Path
+import sys
+import os
+
+PROJECT_DIR = Path(os.path.abspath(__file__)).parent.parent
+sys.path.append(PROJECT_DIR)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'woid.settings')
+
+import django
+django.setup()
+
+
+from twisted.internet import task
+from twisted.internet import reactor
+
+from woid.apps.services.crawlers import RedditCrawler, HackerNewsCrawler, GithubCrawler, MediumCrawler
+
+
+FIVE_MINUTES = 5 * 60
+THIRTY_MINUTES = 30 * 60
+
+def main_loop():
+    print('updating reddit...')
+    RedditCrawler().update_top_stories()
+    print('updating hacker news...')
+    HackerNewsCrawler().update_top_stories()
+    print('updating medium...')
+    MediumCrawler().update_top_stories()
+
+def lazy_loop():
+    print('updating github...')
+    GithubCrawler().update_top_stories()
+
+def main():
+    main_loop_task = task.LoopingCall(main_loop)
+    main_loop_task.start(FIVE_MINUTES)
+
+    main_loop_task = task.LoopingCall(lazy_loop)
+    main_loop_task.start(THIRTY_MINUTES)
+
+    reactor.run()
+
+
+if __name__ == '__main__':
+    main()
